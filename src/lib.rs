38af6397e7fb1c0bcc0015997e6851d2e44e8c5c
Lysander Mealy <lysander.mealy@gmail.com>
2023-08-29 08:41:56 -0400

    chore: :fire: Remove cargo example code

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,1 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
 
-pub fn add_two(x: usize) -> usize {
-    add(x, 2)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
